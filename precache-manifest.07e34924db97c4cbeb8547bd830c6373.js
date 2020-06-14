4a1f0ac18f9d95017770f5b2174367ed7bb71255
neilbc <hai93.bk@gmail.com>
2020-06-15 00:50:24 +0700

    Updates

---
+++ precache-manifest.07e34924db97c4cbeb8547bd830c6373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25599368552d45b16c085a25d7e205e7",
+    "url": "/homeworkReact/index.html"
+  },
+  {
+    "revision": "1498f62bd3b9992773db",
+    "url": "/homeworkReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70ca56a6072219061db5",
+    "url": "/homeworkReact/static/js/2.7153c9fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homeworkReact/static/js/2.7153c9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1498f62bd3b9992773db",
+    "url": "/homeworkReact/static/js/main.3a5a1d5d.chunk.js"
+  },
+  {
+    "revision": "a6c2a2b2a7a5412a2986",
+    "url": "/homeworkReact/static/js/runtime-main.a12e5c44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homeworkReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
